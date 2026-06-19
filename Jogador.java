6efc9b104436874cd5620d8e3181df3b5d3bbec4
Leonardo L. Kologeski <leonardolk212@gmail.com>
2026-06-19 19:41:18 -0300

    I just made some changes to the code. Changes already testet.

--- Jogador.java
+++ Jogador.java
@@ -1,42 +1,58 @@
 public class Jogador{
+
     private String nickname;
-    private Agente[] agentes;
+    private String agente;
     private int kills;
     private int mortes;
     private int assistencias;
     private double kda;
-    public Jogador(String nickname, Agente[] agentes, int kills, int mortes, int assistencias, double kda) {
+
+    public Jogador(String nickname, String agente, int kills, int mortes, int assistencias, double kda) {
+
         this.nickname = nickname;
-        this.agentes = agentes;
+        this.agente = agente;
         this.kills = kills;
         this.mortes = mortes;
         this.assistencias = assistencias;
         this.kda = kda;
     }
+
     public Jogador(){
+
         this.nickname = "";
-        this.agentes = new Agente[0];
+        this.agente = "";
         this.kills = 0;
         this.mortes = 0;
         this.assistencias = 0;
         this.kda = 0.0;
     }
+
     public String getNickname() {
         return nickname;
     }
-    public Agente[] getAgentes() {
-        return agentes;
+
+    public String getAgente() {
+        return agente;
     }
+
     public int getKills() {
         return kills;
     }
+
     public int getMortes() {
         return mortes;
     }
+    
     public int getAssistencias() {
         return assistencias;
     }
+    
     public double getKda() {
         return kda;
+    }
+
+    @Override
+    public String toString(){
+        return "Nickname: " + nickname + ", Agente: " + agente + ", Kills" +  kills + ", Mortes: " + mortes + ", Assistencia: "  + assistencias + ", KDA: " + kda;
     }
 }
--- Main.java
+++ Main.java
@@ -5,39 +5,100 @@ public class Main{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        int opcao = 0;
+
+        int opcao = 0;//Var. 1 switch
+        int opcaoCase2 = 0;//Var. 2 switch
+
+        Jogador jogador[] = new Jogador[3];
+        //Criando jogadores para o switch case 2
+        jogador[0] = new Jogador("Thiikkj", "Naruto", 4, 3, 9, 4.3333333);
+        jogador[1] = new Jogador("leleco", "Itachi", 99, 1, 23, 122.0);
+        jogador[2] = new Jogador("JoPaReis", "Tsunade", 0, 1, 30, 30.0);
+        
+        
+        
         do{
         System.out.println("Gerenciamento de campeonato de Valorant");
         System.out.println("1. cadasrar equipe");
         System.out.println("2. ver estatisticas de jogador");
         System.out.println("3. ver próxima partida");
         System.out.println("4. sair");
-        int opcao1 = scanner.nextInt();
-        switch (opcao1) {
+         opcao = scanner.nextInt();
+
+            switch (opcao) {
             case 1:
                 System.out.print("Digite o nome e a tag da equipe: ");
-                Equipe equipe = new Equipe(scanner.next(), scanner.next(), new Jogador[0]);
+                Equipe equipe = new Equipe(scanner.next(), scanner.next(), null);
+
                 System.out.println("Equipe " + equipe.getNome() + " cadastrada com sucesso!");
+
                 System.out.println("Cadastro de jogadores para a equipe " + equipe.getNome());
+
                 System.out.print("Digite o nome e o agente do jogador: ");
-                Jogador jogador1 = new Jogador(scanner.next(), new Agente[0], 0, 0, 0, 0);
-                System.out.println("Jogador " + jogador1.getNickname() + " cadastrado com sucesso na equipe " + equipe.getNome());
+                
+                
+                System.out.println("Jogador " + jogador[0].getNickname() + " cadastrado com sucesso na equipe " + equipe.getNome());
+
                 break;
+
             case 2:
-                System.out.println("Ver estatísticas de jogador");
+                
+                while (opcaoCase2 != 1){
+                System.out.println("\nJogadores Disponíveis: ");
+                    
+                //aqui vai agregar um número para cada jogador, para o usuário escolher qual jogador ele quer ver as estatísticas.
+                     for (int i = 0; i < jogador.length; i++){
+                    System.out.println("Número: " + i + " - " + jogador[i].getNickname());
+                   }
+
+                //Escolhe o número do jogador para ver as estatísticas
+                    System.out.println("\nEscolha o número do jogador para ver suas estatísticas: "); 
+                    int i= scanner.nextInt();
+
+                    System.out.println(jogador[i].toString());
+                
+
+                System.out.println("1. " + "Sair.");
+                System.out.println("2. " + "Ver estatísticas de outro jogador.");
+                opcaoCase2 = scanner.nextInt();
+
+                switch (opcaoCase2) {
+                    case 1: 
+                    
+                        System.out.println("Saindo...");
+                        break;
+
+                    case 2:
+                        System.out.println("Carregando...");
+                        break;
+                    
+
+
+                default:
+                    System.out.println(")Opção inválida");
+                }
+            }
+                
                 break;
+
             case 3:
                 System.out.println("Ver próxima partida");
                 break;
+
             case 4:
                 System.out.println("Sair");
                 break;
+
             default:
                 System.out.println("Opção inválida");
+            
+
         }
-        }while (opcao != 4);
+            
+        }while(opcao!= 4);
         scanner.close();
         random.doubles();
+        
     }
 }
 
